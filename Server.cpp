7db48615d0ad95eda518211ca86483832bc077b3
Dlyvern <112475993+Dlyvern@users.noreply.github.com>
2023-05-30 16:10:25 +0500

    Update Server.cpp

--- Server.cpp
+++ Server.cpp
@@ -4,35 +4,49 @@ void Server::Connection()
 {
     const int opt = 1; 
     int sock;
-    socklen_t optLen = sizeof(opt);
-    sockaddr_in addr, peer_addr;
-    socklen_t peer_addr_size;
-    
-    peer_addr_size = sizeof(peer_addr);
 
     close(sock); //In case if the old socket is still opened
 
     if(sock = socket(AF_INET, SOCK_STREAM, 0) < 0)
         exit(EXIT_FAILURE);
 
-    bzero((char*)&addr, sizeof(addr));
-
     if (setsockopt(sock, SOL_SOCKET, SO_OOBINLINE, (char*)&opt, sizeof(int)) < 0)  
         exit(EXIT_FAILURE);
+
+    sockaddr_in addr;
+    bzero((char*)&addr, sizeof(addr));
 
     addr.sin_family  = AF_INET;
     addr.sin_port = htons(_port);
     addr.sin_addr.s_addr = inet_addr(_ip);
 
-    if (bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    if (bind(sock, (sockaddr*)&addr, sizeof(addr)) < 0)
         exit(EXIT_FAILURE);
 
-    if(listen(sock, _maxLengthOfConnections) < 0)
-        exit(EXIT_FAILURE);
+    listen(sock, _maxLengthOfConnections);
 
-    if(accept(sock, (struct sockaddr *) &peer_addr, &peer_addr_size) < 0)
-        exit(EXIT_FAILURE);
-        
+    _acceptThread = std::thread([this, sock](){AcceptingClients(sock);});
+    
+    close(sock);
+}
+
+void Server::AcceptingClients(int sock)
+{
+    sockaddr_in newAddr;//To handle a new connection with a client
+    socklen_t newAddrSize = sizeof(newAddr);
+    int newSocket;
+    
+    for(;;)
+    {
+        if(newSocket = accept(sock, (sockaddr *)&newAddr, &newAddrSize) != -1)
+        {
+            _receivingThread = std::thread([this, newSocket](){ReceivingMessages(newSocket);});
+        }
+    }
+}
+
+void Server::ReceivingMessages(int newSocket)
+{
     int bytesRead = 0;
 
     int bytesWritten = 0;
@@ -43,7 +57,8 @@ void Server::Connection()
         
         Packet *packet = new Packet();
 
-        memset(&packet, 0, sizeof(packet));
+        
+
 
         //bytesRead += recv(sock,  sizeof(packet), 0);
         
@@ -51,6 +66,4 @@ void Server::Connection()
 
         //bytesWritten += send(sock, , sizeof(packet), 0);
     }
-
-    close(sock);
 }
