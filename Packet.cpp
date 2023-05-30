a019b0612adbbca3366094d4033cf1edd7e392e2
Dlyvern <112475993+Dlyvern@users.noreply.github.com>
2023-05-30 17:47:54 +0500

    Update Packet.cpp

--- Packet.cpp
+++ Packet.cpp
@@ -8,63 +8,35 @@ Packet::Packet()
     uint16_t _summa = ;
     _data.assign(_lengthOfData, 0);
 }
- uint8_t* Packet::Resize(uint8_t *oldMassiv, uint8_t newElement, int size)
- {
-
-    uint8_t *newMassiv = new uint8_t[size];
-
-if(oldMassiv == nullptr)
-{
-    newMassiv[size - 1] = newElement;
-    uint8_t *newPtr = newMassiv;
-
-    return newMassiv;
-}
-
-    for(int i = 0; i < size; ++i)
-        newMassiv[i] = oldMassiv[i];
-
-    newMassiv[size - 1]  = newElement;
-
-    delete[]oldMassiv;
-
-    uint8_t *newPtr = newMassiv;
-
-    return newMassiv;
- }
 
 uint8_t* Packet::Pack()
 {
-    int size = 1;
-    uint8_t *packedPacket;
-Resize(packedPacket,(uint8_t) _header >> 8, size);
-size++;
+    std::vector<uint8_t>tempPackedPacket;
 
-Resize(packedPacket,(uint8_t) _header &0XFF, size);
-size++;
+    auto x = [&](uint8_t data)->void{tempPackedPacket.push_back(data);};
 
-Resize(packedPacket,_type, size);
-size++;
+    //Splitting data into pieces
+    x((uint8_t) _header >> 8);
+    x((uint8_t) _header &0XFF);
 
-Resize(packedPacket, (uint8_t) _numberOfPacket >> 8, size);
-size++;
+    x(_type);
 
-Resize(packedPacket, (uint8_t) _numberOfPacket &0XFF, size);
-size++;
+    x((uint8_t) _numberOfPacket >> 8);
+    x((uint8_t) _numberOfPacket &0XFF);
 
-Resize(packedPacket, (uint8_t) _lengthOfData >> 8, size);
-size++;
+    x((uint8_t) _lengthOfData >> 8);
+    x((uint8_t) _lengthOfData &0XFF);
+    
+    for(auto it = _data.begin(); it != _data.end(); ++it)
+        x(*it);
 
-Resize(packedPacket, (uint8_t) _lengthOfData &0XFF, size);
-size++;
+    x((uint8_t) _summa >> 8);
+    x((uint8_t) _summa &0XFF);
 
-    auto it = _data.begin();
-    int index = 0;
-    for(; it != _data.end(); ++index, ++it )
-        packedPacket[7 + index] += *it;
+    uint8_t *packedPacket = new uint8_t [tempPackedPacket.size()];
 
-    packedPacket[++index] = (uint8_t) _summa >> 8;
-    packedPacket[++index] = (uint8_t) _summa &0XFF;
+    for(int index = 0; index != tempPackedPacket.size(); ++index)
+        packedPacket[index] = tempPackedPacket.at(index);
 
     return packedPacket;
 }
