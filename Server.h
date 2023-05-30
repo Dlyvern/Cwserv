ce9be6661ea50621c2e6d964be33aebd74113d04
Dlyvern <112475993+Dlyvern@users.noreply.github.com>
2023-05-30 16:10:43 +0500

    Update Server.h

--- Server.h
+++ Server.h
@@ -4,12 +4,25 @@
 #include<arpa/inet.h> 
 #include<iostream>
 #include<unistd.h>
+#include<cstring>
+#include"Packet.h"
+#include<boost/archive/binary_oarchive.hpp>
+#include<boost/archive/binary_iarchive.hpp>
+#include<thread>
 
 class Server
 {
 private:
-    uint64_t _port = 8080;
+    uint16_t _port = 8080;
     const char* _ip = "127.0.0.1";
+    uint64_t _maxLengthOfConnections = 100;
+    std::thread _acceptThread;
+    std::thread _receivingThread;
+
+    void AcceptingClients(int sock);
+
+    void ReceivingMessages(int newSocket);
 public:
     void Connection();
 };
+
