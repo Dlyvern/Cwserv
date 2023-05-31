2843ab36d6ad23f48b0a5e8dca796fb3d74d3c64
Dlyvern <112475993+Dlyvern@users.noreply.github.com>
2023-05-31 10:25:42 +0500

    Update Client.cpp

--- Client.cpp
+++ Client.cpp
@@ -1,17 +1,43 @@
 #include"Client.h"
 
 void Client::Connection()
-{
-    sockaddr_in addr; 
-    
+{  
     int sock;
+
+    close(sock);
+
     if(sock = socket(AF_INET, SOCK_STREAM, 0) < 0)
         exit(EXIT_FAILURE);
 
+    sockaddr_in addr; 
+    bzero((char*)&addr, sizeof(addr));
     addr.sin_family = AF_INET; 
     addr.sin_port = htons(_port);
     addr.sin_addr.s_addr = inet_addr(_ip);
+    int socketForConnection;
+    
+    for(;;)
+    {
+        if(socketForConnection = connect(sock, (sockaddr *)&addr, sizeof(addr)) != 1)
+            break;
+    }
 
-    if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-        exit(EXIT_FAILURE);
+    char msg[1500]; 
+    std::string data = "Hello";
+    int bytesWritten = 0;
+    int bytesRead = 0;
+
+    while(1)
+    {
+        memset(&msg, 0, sizeof(msg));
+        
+        strcpy(msg, data.c_str());
+
+        bytesWritten += send(socketForConnection, (char*)&msg, strlen(msg), 0);
+
+        bytesRead += recv(socketForConnection, (char*)&msg, sizeof(msg), 0);
+    }
+
+    close(socketForConnection);
+    close(sock);
 }
