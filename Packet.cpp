335aa81a9b2b148372b6759c28ec378e4d8781cc
Dlyvern <112475993+Dlyvern@users.noreply.github.com>
2023-05-31 10:12:35 +0500

    Update Packet.cpp

--- Packet.cpp
+++ Packet.cpp
@@ -1,31 +1,5 @@
 #include"Packet.h"
 
-Packet::Packet()
-{
-    _type = 0;
-    _numberOfPacket = 0;
-    _lengthOfData = 0;
-
-    uint8_t *tmp = new uint8_t[7];
-
-    tmp[0] = (uint8_t) _header >> 8;
-    tmp[1] = (uint8_t) _header &0XFF;
-
-    tmp[2] = _type;
-
-    tmp[3] = (uint8_t) _numberOfPacket >> 8;
-    tmp[4] = (uint8_t) _numberOfPacket &0XFF; 
-
-    tmp[5] = (uint8_t) _lengthOfData >> 8;
-    tmp[6] = (uint8_t) _lengthOfData &0XFF; 
-
-    _summa = CRC16_2(tmp, 7);
-
-    delete[] tmp;
-
-    _data.assign(_lengthOfData, 0);
-}
-
 unsigned int CRC16_2(unsigned char *buf, int len)
 {  
   unsigned int crc = 0xFFFF;
@@ -46,49 +20,49 @@ unsigned int CRC16_2(unsigned char *buf, int len)
       crc >>= 1;                    // Just shift right
     }
   }
-
   return crc;
+}
+
+uint8_t* Packet::MakeAPacketForCalculatingSRC()
+{
+  uint8_t *arrayForCalculatingCRC{new uint8_t[7]
+  {
+    (uint8_t) _header >> 8, 
+    (uint8_t) _header &0XFF, 
+    _type, 
+    (uint8_t) _numberOfPacket >> 8, 
+    (uint8_t) _numberOfPacket &0XFF, 
+    (uint8_t) _lengthOfData >> 8,
+    (uint8_t) _lengthOfData &0XFF
+  }}; 
+
+  for(int index = 0; index < _data.size(); ++index)
+    arrayForCalculatingCRC[index + 7] = _data.at(index);
+
+  return arrayForCalculatingCRC;
+}
+
+Packet::Packet()
+{
+  _type = 0;
+  _numberOfPacket = 0;
+  _lengthOfData = 0;
+  _summa = CRC16_2(MakeAPacketForCalculatingSRC(), 7);
+  _data.assign(_lengthOfData, 0);
 }
 
 uint8_t* Packet::Pack()
 {
-    std::vector<uint8_t>tempPackedPacket;
+    uint8_t *packedPacket = MakeAPacketForCalculatingSRC();
 
-    auto x = [&](uint8_t data)->void{tempPackedPacket.push_back(data);};
+    int size = sizeof(packedPacket) / sizeof(uint8_t);
 
-    //Splitting data into pieces
-    x((uint8_t) _header >> 8);
-    x((uint8_t) _header &0XFF);
-
-    x(_type);   
-
-    x((uint8_t) _numberOfPacket >> 8);
-    x((uint8_t) _numberOfPacket &0XFF);
-
-    x((uint8_t) _lengthOfData >> 8);
-    x((uint8_t) _lengthOfData &0XFF);
-    
-    for(auto it = _data.begin(); it != _data.end(); ++it)
-        x(*it);
-
-    uint8_t *src = new uint8_t[tempPackedPacket.size()];
-
-    for(int index = 0; index != tempPackedPacket.size(); ++index)
-        src[index] = tempPackedPacket.at(index);
-
-    x(CRC16_2(src, tempPackedPacket.size()));
-
-    delete[]src;
-
-    uint8_t *packedPacket = new uint8_t [tempPackedPacket.size()];
-
-    for(int index = 0; index != tempPackedPacket.size(); ++index)
-        packedPacket[index] = tempPackedPacket.at(index);
+    packedPacket[size] = ((CRC16_2(packedPacket, size)));
 
     return packedPacket;
 }
 
 void Packet::Unpack(uint8_t *data)
 {
-    if(data[0] != _header) throw "INVALID DATA";
+
 }
