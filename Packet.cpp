689fd345e0d64e5eb99eecdbee936b66dadab9ef
Dlyvern <112475993+Dlyvern@users.noreply.github.com>
2023-06-01 14:47:04 +0500

    Update Packet.cpp

--- Packet.cpp
+++ Packet.cpp
@@ -1,61 +1,36 @@
 #include"Packet.h"
 
-unsigned int CRC16_2(unsigned char *buf, int len)
-{  
-  unsigned int crc = 0xFFFF;
-
-  for (int pos = 0; pos < len; pos++)
-  {
-
-  crc ^= (unsigned int)buf[pos];    // XOR byte into least sig. byte of crc
-
-  for (int i = 8; i != 0; i--) 
-  {    // Loop over each bit
-    if ((crc & 0x0001) != 0)
-    {      // If the LSB is set
-      crc >>= 1;                    // Shift right and XOR 0xA001
-      crc ^= 0xA001;
-    }
-    else                            // Else LSB is not set
-      crc >>= 1;                    // Just shift right
-    }
-  }
-  return crc;   
-}
-
-uint8_t* Packet::MakeAPacketForCalculatingSRC()
+void Packet::MakeAPacketForCalculatingSRC(std::vector<uint8_t>&arrayForCalculatingCRC)
 {
-  uint8_t *arrayForCalculatingCRC{new uint8_t[7]
+  arrayForCalculatingCRC = 
   {
-    (uint8_t) _header >> 8, 
-    (uint8_t) _header &0XFF, 
+    {static_cast<uint8_t>(_header >> 8)},
+    {static_cast<uint8_t>(_header &0XFF)}, 
     _type, 
-    (uint8_t) _numberOfPacket >> 8, 
-    (uint8_t) _numberOfPacket &0XFF, 
-    (uint8_t) _lengthOfData >> 8,
-    (uint8_t) _lengthOfData &0XFF
-  }}; 
+    {static_cast<uint8_t>(_numberOfPacket >> 8)},
+    {static_cast<uint8_t>(_numberOfPacket &0XFF)},
+    {static_cast<uint8_t>(_lengthOfData >> 8)},
+    {static_cast<uint8_t>( _lengthOfData &0XFF)}
+  };
 
-  for(int index = 0; index < _data.size(); ++index)
-    arrayForCalculatingCRC[index + 6] = _data.at(index);
-
-  return arrayForCalculatingCRC;
+  for(const auto&it : _data)
+    arrayForCalculatingCRC.push_back(it);
 }
 
 Packet::Packet(std::vector<uint8_t>&data)
 {
     _type = data.at(2);
 
-    _numberOfPacket = (uint16_t) data[3] << 8 | data[4];
+    _numberOfPacket = {static_cast<uint16_t>(data.at(3) << 8 | data.at(4))};
 
-    _lengthOfData = (uint16_t) data[5] << 8 | data[6];
+    _lengthOfData = {static_cast<uint16_t>(data.at(5) << 8 | data.at(6))};
 
     int index = 0;
     
-    for(; index < _lengthOfData; ++index)
-        _data.at(index) = data[index + 7]; //Data begins at 7 index
+    for(; index < _lengthOfData - 1; ++index)
+        _data.push_back(data[index + 7]);
 
-    _summa = (uint16_t) data[index + 7] << 8 | data[index + 8];
+    _summa = {static_cast<uint16_t>(data.at(index + 7) << 8 | data.at(index + 8))};
 }
 
 Packet::Packet()
@@ -63,36 +38,41 @@ Packet::Packet()
   _type = 0;
   _numberOfPacket = 0;
   _lengthOfData = 0;
-  _summa = CRC16_2(MakeAPacketForCalculatingSRC(), 6);
+  std::vector<uint8_t>data;
+  MakeAPacketForCalculatingSRC(data);
+  _summa = CRC16_2(data);
   _data.assign(_lengthOfData, 0);
 }
 
-uint8_t* Packet::Pack()
+void Packet::Pack(std::vector<uint8_t>&packedPacket )
 {
-    uint8_t *packedPacket = MakeAPacketForCalculatingSRC();
+    MakeAPacketForCalculatingSRC(packedPacket);
 
-    int size = sizeof(packedPacket) / sizeof(uint8_t);
+    uint16_t crc = (CRC16_2(packedPacket));
 
-    packedPacket[size] = ((CRC16_2(packedPacket, size)));
+    uint8_t c = {static_cast<uint8_t> (crc >> 8)};
+    uint8_t rc = {static_cast<uint8_t> (crc &0XFF)};
 
-    return packedPacket;
+
+    packedPacket.push_back(c);
+    packedPacket.push_back(rc);
 }
 
-void Packet::Unpack(uint8_t *data)
+void Packet::Unpack(std::vector<uint8_t>&data)
 {
-    if(data[0] != ((uint8_t) _header >> 8))
+    if(data.at(0) != ((uint8_t) _header >> 8))
       throw "INVALID DATA";
     
-    _type = data[2];
+    _type = data.at(2);
 
-    _numberOfPacket = (uint16_t) data[3] << 8 | data[4];
+    _numberOfPacket = {static_cast<uint16_t>(data.at(3) << 8 | data.at(4))};
 
-    _lengthOfData = (uint16_t) data[5] << 8 | data[6];
+    _lengthOfData = {static_cast<uint16_t>(data.at(5) << 8 | data.at(6))};
 
     int index = 0;
 
     for(; index < _lengthOfData; ++index)
-        _data.at(index) = data[index + 7]; //Data begins at 7 index
+        _data.push_back(data[index + 7]); //Data begins at 7 index
 
-    _summa = (uint16_t) data[index + 7] << 8 | data[index + 8];
+    _summa = {static_cast<uint16_t>(data.at(index + 7) << 8 | data.at(index + 8))};
 }
