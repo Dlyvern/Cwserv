734b22222eaa7e1de5043ac344291072273c4e5e
Dlyvern <112475993+Dlyvern@users.noreply.github.com>
2023-06-02 15:28:21 +0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,60 +1,40 @@
-#include<thread>
-#include"Server.h"
-#include"Client.h"
+#include <thread>
+#include "Server.h"
+#include "Client.h"
 
-unsigned int CRC16_2(std::vector<uint8_t>&buf)
-{  
-  unsigned int crc = 0xFFFF;
+std::string ByteToHex(std::vector<uint8_t> &data)
+{
+  std::stringstream ss;
 
-  for (const auto&it : buf)
-  {
+  for (int i = 0; i < data.size(); ++i)
+    ss << "0x" << std::setfill('0') << std::setw(2) << std::right << std::hex << (unsigned int)data[i] << ' ';
 
-  crc ^= (unsigned int)it;    // XOR byte into least sig. byte of crc
+  std::string result;
 
-  for (int i = 8; i != 0; i--) 
-  {    // Loop over each bit
-    if ((crc & 0x0001) != 0)
-    {      // If the LSB is set
-      crc >>= 1;                    // Shift right and XOR 0xA001
-      crc ^= 0xA001;
-    }
-    else                            // Else LSB is not set
-      crc >>= 1;                    // Just shift right
-    }
-  }
-  return crc;   
+  result = ss.str();
+
+  return result;
 }
 
-std::string ByteToHex(std::vector<uint8_t>&data)
+void ClientThread()
 {
-    std::stringstream ss;
-
-    for(int i = 0; i < data.size(); ++i)
-        ss <<  "0x" << std::setfill('0') << std::setw(2)  << std::right << std::hex << (unsigned int) data[i] << ' ';
-
-    std::string result;
-
-    result = ss.str();
-
-    return result;
+  Client *client = new Client();
+  client->Connection();
 }
 
-void CreateServer()
+void ServerThread()
 {
-    Server* server = new Server();
-    server->Connection();
-    delete(server);
+  Server *server = new Server();
+  server->Connection();
 }
 
 int main()
 {
-    std::thread thServer(CreateServer);
-
-    Client*client = new Client();
-
-    client->Connection();
-    
-    delete(client);
-    
-    return {};
+  std::thread th2(ServerThread);
+  th2.detach();
+  
+  std::thread th(ClientThread);
+  th.join();
+  
+  return {};
 }
