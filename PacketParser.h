db220bbe698eb0643378d86d89a6eabd992eceaf
Dlyvern <112475993+Dlyvern@users.noreply.github.com>
2023-06-02 15:26:48 +0500

    Update PacketParser.h

--- PacketParser.h
+++ PacketParser.h
@@ -1,11 +1,31 @@
+#ifndef PACKET_PARSER_H
+#define PACKET_PARSER_H
 #include<vector>
-#include<Packet.h>
+#include<stdint.h>
+#include"Packet.h"
+#include<thread>
+#include<mutex>
 
 class PacketParser
 {
 private:
-    std::vector<Packet*>_packets;
+    std::vector<uint8_t>*_input;
+    std::vector<Packet*>_output;
+    std::thread _workingThread;
+    std::mutex *_mutex;
+    uint64_t _damagePacketsCounter;
+    uint64_t _successfulPackets;
+    bool _run;
 
+    void Run();
+
+    void AddASuccessfulPacketToTheQueue(Packet*packet);
 public:
+    PacketParser(std::vector<uint8_t>*input, std::mutex *mutex);
 
+    void StartProcess();
+
+    void StopProceess();
 };
+
+#endif
