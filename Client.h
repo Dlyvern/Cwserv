768af30768ce6832b9f307ef86fe176005804fd8
Dlyvern <112475993+Dlyvern@users.noreply.github.com>
2023-06-02 15:27:46 +0500

    Update Client.h

--- Client.h
+++ Client.h
@@ -1,5 +1,13 @@
 #ifndef CLIENT_H
 
+#ifndef PACKET_PARSER_H
+#include"PacketParser.h"
+#endif
+
+#ifndef PACKET_H
+#include"Packet.h"
+#endif
+
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
@@ -7,12 +15,15 @@
 #include<unistd.h>
 #include<string.h>
 #include<thread>
-#include"PacketParser.h"
 #include<algorithm>
 #include <sstream>
 #include <cstring>
 #include<iostream>
 #include<iomanip>
+#include<unordered_map>
+#include<random>
+
+#include<mutex>
 
 std::string ByteToHex(std::vector<uint8_t>&data);
 
@@ -21,8 +32,11 @@ class Client
 private:
     uint16_t _port = 8080;
     const char* _ip = "127.0.0.1";
+    // "192.168.31.152";
+    //"127.0.0.1";
     std::thread _sendingThread;
     std::thread _receivingThread;
+    void SendMessage(int&sock);
  public:
     void Connection();  
 };
