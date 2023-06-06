e49c1151c6a1e4bcc8f1e3852db4ce54e536f083
Dlyvern <112475993+Dlyvern@users.noreply.github.com>
2023-06-06 15:19:37 +0500

    Update Server.h

--- Server.h
+++ Server.h
@@ -4,6 +4,10 @@
 #include"PacketParser.h"
 #endif
 
+#ifndef CLIENT_H
+#include"Client.h"
+#endif
+
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
@@ -14,21 +18,42 @@
 #include<thread>
 #include<vector>
 #include<mutex>
+#include<memory>
 
 class Server
 {
 private:
-    int16_t _port = 8080;
-    const char* _ip = "127.0.0.1";
-    uint64_t _maxLengthOfConnections = 100;
+    int16_t _port;
+    const char* _ip;
+     static bool _work;
+    static unsigned int _clientId;
+    int _sock;
+    struct sockaddr_in _addrInfo;
+    socklen_t _sizeAddrInfo;
+    int _maxNumberOfConnections;
+
     std::thread _acceptThread;
     std::vector<std::thread>_receivingMessagesThread;
     std::mutex _mutex;
-    void AcceptingClients(int sock);
+    std::vector<std::unique_ptr<Client>>_clients;
+
+    void AcceptingClients();
 
     void ReceivingMessages(int newSocket);
+
+    void Initialization();
+
+    std::unique_ptr<Client> FindClient(int id);
+
 public:
-    void Connection();
+
+    void Start();
+    void Stop();
+    bool DisconnectClient(int id);
+    void DisconnectClients();
+
+    Server(int port, std::string ip, int maxNumberOfConnections);
+    ~Server();
 };
 
 #endif
